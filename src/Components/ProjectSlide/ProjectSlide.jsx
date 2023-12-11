a14215ccae0f5d85fafc5e9914d134de9b89b358
Devarshi <devarshidwi@gmail.com>
2023-12-12 04:49:08 +0530

    Removed Vercel Analytics, minor bug fixes

--- src/Components/ProjectSlide/ProjectSlide.jsx
+++ src/Components/ProjectSlide/ProjectSlide.jsx
@@ -1,63 +1,68 @@
-import { useEffect, useRef, Fragment } from "react"
-import { FiGithub, FiExternalLink } from "react-icons/fi"
-import "./projectSlide.css"
+import { useEffect, useRef, Fragment } from 'react';
+import { FiGithub, FiExternalLink } from 'react-icons/fi';
+import './projectSlide.css';
 
 function ProjectSlide({
-    name,
-    projectURL,
-    targetSelf,
-    projectImgURL,
-    alt_text,
-    icons,
-    repoURL,
+	name,
+	projectURL,
+	targetSelf,
+	projectImgURL,
+	altText,
+	icons,
+	repoURL,
 }) {
-    const cardWrapper = useRef()
+	const cardWrapper = useRef();
 
-    useEffect(() => {
-        const observer = new IntersectionObserver(
-            (entries) => {
-                entries[0].target.classList.toggle(
-                    "show",
-                    entries[0].isIntersecting
-                )
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			(entries) => {
+				entries[0].target.classList.toggle('show', entries[0].isIntersecting);
 
-                if (entries[0].isIntersecting)
-                    observer.unobserve(entries[0].target)
-            },
-            { threshold: 0.2 }
-        )
+				if (entries[0].isIntersecting) observer.unobserve(entries[0].target);
+			},
+			{ threshold: 0.2 }
+		);
 
-        observer.observe(cardWrapper.current)
+		observer.observe(cardWrapper.current);
 
-        return () => observer.disconnect()
-    }, [])
+		return () => observer.disconnect();
+	}, []);
 
-    return (
-        <div className='cardWrapper' ref={cardWrapper}>
-            <div className='card'>
-                <img src={projectImgURL} alt={alt_text} />
-                <div className='card-info'>
-                    <div className='icons'>
-                        {icons.map((icon, i) => {
-                            const getIcon = () => icon
-                            return <Fragment key={i}>{getIcon()}</Fragment>
-                        })}
-                    </div>
-                </div>
-            </div>
-            <div className='iconsCont'>
-                <a href={repoURL} target='_blank'>
-                    <FiGithub />
-                </a>
-                <p>{name}</p>
-                <a
-                    href={projectURL}
-                    target={`${targetSelf ? "_self" : "_blank"}`}>
-                    <FiExternalLink />
-                </a>
-            </div>
-        </div>
-    )
+	console.log(altText);
+
+	return (
+		<div
+			className='cardWrapper'
+			ref={cardWrapper}>
+			<div className='card'>
+				<img
+					src={projectImgURL}
+					alt={altText}
+				/>
+				<div className='card-info'>
+					<div className='icons'>
+						{icons.map((icon, i) => {
+							const getIcon = () => icon;
+							return <Fragment key={i}>{getIcon()}</Fragment>;
+						})}
+					</div>
+				</div>
+			</div>
+			<div className='iconsCont'>
+				<a
+					href={repoURL}
+					target='_blank'>
+					<FiGithub />
+				</a>
+				<p>{name}</p>
+				<a
+					href={projectURL}
+					target={`${targetSelf ? '_self' : '_blank'}`}>
+					<FiExternalLink />
+				</a>
+			</div>
+		</div>
+	);
 }
 
-export default ProjectSlide
+export default ProjectSlide;
